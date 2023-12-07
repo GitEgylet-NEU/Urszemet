0e7df1176601e8080c3ac5065aaa1bdc559ce7b8
zuipzuip <momarci0631@gmail.com>
2023-12-07 23:57:21 +0100

    speedran
    holo animation

--- Assets/UIcontroller.cs
+++ Assets/UIcontroller.cs
@@ -1,4 +1,4 @@
-using JetBrains.Annotations;
+ï»¿using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -8,105 +8,106 @@ using UnityEngine.UIElements;
 
 public class UIcontroller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public bool HoloActive = false;
-    public AnimationClip InClip;
-    public int ActiveTab = 0;
-    public Animator HoloAnimator;
-    public GameObject HoloObject;
-    public AnimationClip clip;
-    public GameObject[] UI;
+	// Start is called before the first frame update
+	public bool HoloActive = false;
+	public AnimationClip InClip;
+	public int ActiveTab = 0;
+	public Animator HoloAnimator;
+	public GameObject HoloObject;
+	public AnimationClip clip;
+	public GameObject[] UI;
 
-    void Start()
-    {
-        ActiveTab = -1;
-        //HoloAnimator = HoloObject.GetComponent<Animator>();
-        //AnimationCurve curve = new AnimationCurve();
-        //curve.AddKey(0.5f, 1f);
+	void Start()
+	{
+		ActiveTab = -1;
+		//HoloAnimator = HoloObject.GetComponent<Animator>();
+		//AnimationCurve curve = new AnimationCurve();
+		//curve.AddKey(0.5f, 1f);
 
-        //curve.CopyFrom(AnimationUtility.GetEditorCurve(InClip, "", typeof(RectTransform), "Position.x"));
-        //curve.CopyFrom(AnimationUtility.GetAllCurves(InClip));
-        //AnimationUtility.GetAllCurves(InClip);
-        
-        //clip = EditorGUILayout.ObjectField("Clip",clip,typeof(AnimationClip), false) as AnimationClip;
-        //curve.CopyFrom(AnimationUtility.GetObjectReferenceCurve(InClip,));
-        //Debug.Log(curve.keys.Length);
-        //foreach (AnimationCurve key in AnimationUtility.GetCurveBindings(InClip))
-        //{
-        //    Debug.Log(key);
-        //}
+		//curve.CopyFrom(AnimationUtility.GetEditorCurve(InClip, "", typeof(RectTransform), "Position.x"));
+		//curve.CopyFrom(AnimationUtility.GetAllCurves(InClip));
+		//AnimationUtility.GetAllCurves(InClip);
 
-       //InClip.SetCurve("",typeof(RectTransform), "Position.x", curve);
-    }
+		//clip = EditorGUILayout.ObjectField("Clip",clip,typeof(AnimationClip), false) as AnimationClip;
+		//curve.CopyFrom(AnimationUtility.GetObjectReferenceCurve(InClip,));
+		//Debug.Log(curve.keys.Length);
+		//foreach (AnimationCurve key in AnimationUtility.GetCurveBindings(InClip))
+		//{
+		//    Debug.Log(key);
+		//}
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
-    public void PlayButton()
-    {
-        SceneManager.LoadScene("SampleScene");
-    }
-    public void Changed() 
-    {
-       
-    }
+		//InClip.SetCurve("",typeof(RectTransform), "Position.x", curve);
+	}
 
-    public void ManualButtonFunc()
-    {
-        ActiveTab = 1;
-        Debug.Log("Manual");
-    }
+	// Update is called once per frame
+	void Update()
+	{
+	}
+	public void PlayButton()
+	{
+		SceneManager.LoadScene("SampleScene");
+	}
+	public void Changed()
+	{
 
-    public void ShopButtonFunc()
-    {
-        //if (ActiveTab == 2)
-        //{
-        //    HoloAnimator.SetBool("open", false);
-        //    ActiveTab = 0;
-        //    Debug.Log("Close");
-        //    HoloActive = false;
-        //}
-        //else 
-        //{
+	}
 
-        //    if (HoloActive == false)
-        //    {
-        //        HoloAnimator.SetBool("open", true);
-        //        HoloActive = true;
-        //    }
+	public void ManualButtonFunc()
+	{
+		ActiveTab = 1;
+		Debug.Log("Manual");
+	}
 
-        //    ActiveTab = 2;
-        //    Debug.Log("Shop");
-        //}
-    }
-    //gombonyáskor meghív int button sorszámmal
-    public void HoloMove( int pressed) {
-        if (pressed != ActiveTab)
-        {
-            //megnyitni animálva, nincs meg
-            if (ActiveTab == -1)
-            {
-                //animálás
-                HoloObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, Screen.width, 0);
-            }
-            else
-            {
-                UI[ActiveTab].SetActive(false);
-            }
-            // active ui window state megváltoztatása
-            
-            UI[pressed].SetActive(true);
-            ActiveTab = pressed;
-            
-        }
-        else
-        {
-            //újra megnyomja: bezáródik
-            UI[ActiveTab].SetActive(false);
-            ActiveTab = -1;
-        }
+	public void ShopButtonFunc()
+	{
+		//if (ActiveTab == 2)
+		//{
+		//    HoloAnimator.SetBool("open", false);
+		//    ActiveTab = 0;
+		//    Debug.Log("Close");
+		//    HoloActive = false;
+		//}
+		//else 
+		//{
 
-    }
+		//    if (HoloActive == false)
+		//    {
+		//        HoloAnimator.SetBool("open", true);
+		//        HoloActive = true;
+		//    }
+
+		//    ActiveTab = 2;
+		//    Debug.Log("Shop");
+		//}
+	}
+	//gombonyï¿½skor meghï¿½v int button sorszï¿½mmal
+	public void HoloMove(int pressed)
+	{
+		if (pressed != ActiveTab)
+		{
+			//megnyitni animï¿½lva, nincs meg
+			if (ActiveTab == -1)
+			{
+				//animï¿½lï¿½s
+				HoloObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, Screen.width, 0);
+			}
+			else
+			{
+				UI[ActiveTab].SetActive(false);
+			}
+			// active ui window state megvï¿½ltoztatï¿½sa
+
+			UI[pressed].SetActive(true);
+			ActiveTab = pressed;
+
+		}
+		else
+		{
+			//ï¿½jra megnyomja: bezï¿½rï¿½dik
+			UI[ActiveTab].SetActive(false);
+			ActiveTab = -1;
+		}
+
+	}
 
 }
